07ac4166eb9f07cba868cd06dc1c636e20a93212
Newton McCollum <newton.mccollum@jhuapl.edu>
2021-04-01 14:05:43 -0400

    configure host script

--- configure-host-os.sh
+++ configure-host-os.sh
@@ -9,22 +9,83 @@ echo "Apt Update"
 apt update
 apt install -y --no-install-recommends git
 
-echo "Installing Docker / Docker-Compose"
-apt install -y --no-install-recommends docker docker-compose 
+# Access Point Update
+echo "Configuring Access Point"
+apt install hostapd dnsmasq bridge-utils
+systemctl stop hostapd
+systemctl stop dnsmasq
 
+cat <<EOT >> /etc/dhcpd.conf
+interface wlan0
+static ip_address=192.168.0.10/24
+domain=wlan
+address=/gw.wlan/192.168.0.1
+EOT
+
+mv /etc/dnsmasq.conf /etc/dnsmasq.conf.orig
+cat <<EOT >> /etc/dnsmasq.conf
+interface=wlan0
+  dhcp-range=192.168.0.11,192.168.0.30,255.255.255.0,24h
+EOT
+
+cat <<EOT >> /etc/hostapd/hostapd.conf
+interface=wlan0
+hw_mode=g
+channel=7
+wmm_enabled=0
+macaddr_acl=0
+auth_algs=1
+ignore_broadcast_ssid=0
+wpa=2
+wpa_key_mgmt=WPA-PSK
+wpa_pairwise=TKIP
+rsn_pairwise=CCMP
+ssid=pinet
+wpa_passphrase=pinet1234!
+EOT
+
+echo "DAEMON_CONF=\"/etc/hostapd/hostapd.conf\"" >> /etc/default/hostapd
+
+
+echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
+
+iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+sudo sh -c "iptables-save > /etc/iptables.ipv4.nat"
+cat <<EOT >> /etc/rc.local
+iptables-restore < /etc/iptables.ipv4.nat
+exit 0
+EOT
+
+sudo brctl addbr br0
+sudo brctl addif br0 eth0
+cat <<EOT >> /etc/network/interfaces
+auto br0
+iface br0 inet manual
+bridge_ports eth0 wlan0
+EOT
+
+# Docker/Compose Update
+echo "Installing Docker / Docker-Compose"
+apt install -y --no-install-recommends docker docker-compose
+
+# Add Video Tools
 echo "Installing Video Nice To Haves"
 apt install -y --no-install-recommends v4l-utils
 
-echo "Configuring GPSD Device"
+# Remove GPSD Installed by default
+echo "Removing HOST GPSD Device"
 apt remove -y gpsd
 
+# Clone the Repo
 echo "Cloning REPO"
 cd /opt
 git clone https://github.com/Newton-17/en605.715-drone-sensor-suite.git
 
+# Build out the code
 echo "Building Repo"
 cd /opt/en605.715-drone-sensor-suite
 docker-compose build
 
+# Start the code
 echo "Starting Streaming Applications"
 docker-compose up
